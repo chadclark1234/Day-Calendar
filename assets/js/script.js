89d2e6646a9049a7ba04bd884872d78613b43ac1
Chad Clark <chadclark12@hotmail.com>
2021-01-11 22:42:32 -0600

    docs:addJS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,45 @@ $("#currentDay").html(moment().format("dddd MMMM Do YYYY"));
 //BUILD OUT ROW/COLUMNS
 for (let hour = 9; hour <= 17; hour++) {
   let index = hour - 9;
+
   // ROWS
   let $rowDiv = $("<div>");
   $rowDiv.addClass("row");
   $rowDiv.addClass("plannerRow");
   $rowDiv.attr("data-hour", hour);
+
   // COLUMNS FOR TIME
   let $colTimeDiv = $("<div>");
   $colTimeDiv.addClass("col-md-2");
   $colTimeDiv.addClass("hour");
+
+  //CALCULATE 24HR TIME INTO 12HR AM/PM
+  const time = hour > 12 ? hour - 12 : hour;
+  const amPM = hour >= 12 ? `${time} pm` : `${time} am`;
+
+  // SPAN TO DISPLAY TIME IN DIV
+  let $timeSpan = $("<span>");
+  $timeSpan.attr("class", "time");
+  $timeSpan.html(amPM);
+
   //COLUMNS FOR TEXT
-  let $colTextDiv = $("<div>");
+  let $colTextDiv = $("<textarea>");
   $colTextDiv.addClass("col-md-9");
   $colTextDiv.addClass("textarea");
+
   //COLUMNS FOR SAVE ICON
   let $colSaveDiv = $("<div>");
   $colSaveDiv.addClass("col-md-1");
   $colSaveDiv.addClass("saveBtn");
+
   // APPEND NEW ITEMS
   $(".container").append($rowDiv);
   $rowDiv.append($colTimeDiv);
   $rowDiv.append($colTextDiv);
   $rowDiv.append($colSaveDiv);
+  $colTimeDiv.append($timeSpan);
+
+  console.log(time);
 }
 
 console.log(moment().format("H"));
